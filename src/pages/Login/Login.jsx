64b7cb628185e9902006671fe24af9b9cdd7910b
hiTech <braykul2605@gmail.com>
2024-11-11 10:35:38 +0700

    add initial pages and routing for Chat, Login, and ProfileUpdate

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div class='login'>
+        <img src="" alt="" className='logo' />
+
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default ProfileUpdate
