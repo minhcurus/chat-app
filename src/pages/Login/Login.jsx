0e08f125ccc89fb277d3317401e51395ad256c8e
hiTech <braykul2605@gmail.com>
2024-11-11 15:25:02 +0700

    add Login page with responsive design and toggle between Sign up and Login states

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState("Sign up")
+
   return (
     <div class='login'>
-        <img src="" alt="" className='logo' />
+        <img src={assets.logo_big} alt="" className='logo' />
+        <form className='login-form'>
+          <h2>{currState}</h2>
+          {currState=="Sign up" ?<input type="text" placeholder='username' className="form-input" required />:null}
+          <input type="email" placeholder='Email address' className="form-input" required />
+          <input type="password" placeholder='password' className="form-input" required />
+          <button type='submit'>{currState == "Sign up"?"Create account": "Login now"}</button>
+          <div className='login-term'>
+            <input type="checkbox" />
+            <p>Agree to the terms of use & privacy policy.</p>
+          </div>
+          <div className="login-forgot">
+            {
+              currState == "Sign up"
+              ? <p className='login-toggle'>Already have an account <span onClick={()=>setCurrState("Login")}>click here</span> </p>
+              : <p className='login-toggle'>Create an account <span onClick={()=>setCurrState("Sign up")}>click here</span></p>
+            }
+          </div>
+        </form>
 
     </div>
   )
