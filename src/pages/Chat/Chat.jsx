07b7e0f059b64455b6a8a9e758f809768b6b63c9
hiTech <braykul2605@gmail.com>
2024-11-12 16:34:36 +0700

    add ChatBox and LeftSidebar components with styles and layout integration

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Chat.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import ChatBox from "../../components/ChatBox/ChatBox";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 
 const Chat = () => {
   return (
     <div className="chat">
-      <div className="chat-controller">
-        
+      <div className="chat-container">
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
       </div>
     </div>
   );
