9c96642a2e3af8277167c5fe70a4a1be00474206
hiTech <braykul2605@gmail.com>
2024-11-18 10:36:09 +0700

    enhance ChatBox component with message display and input functionality

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -6,8 +6,39 @@ const ChatBox = () => {
     <div className='chat-box'>
         <div className="chat-user">
             <img src={assets.profile_img} alt="" />
-            <p>Quang Minh <img src={assets.green_dot} alt="" /></p>
+            <p>Quang Minh <img className='dot' src={assets.green_dot} alt="" /></p>
             <img src={assets.help_icon} className='help' alt="" />
+        </div>
+
+
+        <div className="chat-msg">
+          <div className="s-msg">
+            <p className="msg">Lorem ipsum dolor mpora fugit totam. Amet?</p>
+            <div>
+              <img src={assets.profile_img} alt="" />
+              <p>2:30 PM</p>
+            </div>
+          </div>
+          <div className="r-msg">
+            <p className="msg">Lorem ipsum dolor sit amet, lqfugit totam. Amet?</p>
+            <div>
+              <img src={assets.profile_img} alt="" />
+              <p>2:30 PM</p>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+        <div className="chat-input">
+          <input type="text" placeholder='Send a message' />
+          <input type="file" id='image' accept='image/png, image/jped' hidden />
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt="" />
+          </label>
+          <img src={assets.send_button} alt="" />
         </div>
 
     </div>
